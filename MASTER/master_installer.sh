2e8f25e1abb549b8824ece3a56eb7f52b53e5318
Dariusz Fojk <dariuszfojk@gmail.com>
2026-05-15 13:00:14 +0200

    release(MASTER): master-1.2.50

--- MASTER/master_installer.sh
+++ MASTER/master_installer.sh
@@ -37,7 +37,7 @@ set -euo pipefail 2>/dev/null || set -eu
 # - Cleans downloaded *.sh from TMP at the end (asks).
 # - Bash backups are kept as single .bak files (no timestamp pile-up).
 # ==========================================================
-MASTER_VERSION="1.2.49"
+MASTER_VERSION="1.2.50"
 
 # >>> AUTO-MODULE-VERSIONS START >>>
 STATUS_VERSION="3.12.16"
@@ -45,7 +45,7 @@ CLEANUP_VERSION="1.0.3"
 TSEQ_VERSION="3.12.9"
 DOWNLOADER_APP_VERSION="1.0.4"
 UPGBUILDER_VERSION="0.1.11"
-SERVICEGUARD_VERSION="0.1.2"
+SERVICEGUARD_VERSION="0.1.3"
 
 MODE="install"
 UPDATE_ONLY_MODE="0"
