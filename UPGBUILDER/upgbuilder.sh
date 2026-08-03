d13333d69249d088de02aa559276d22b2690b15b
Dariusz Fojk <dariuszfojk@gmail.com>
2026-08-03 23:06:45 +0200

    release(UPGBUILDER): upgbuilder-0.1.14

--- UPGBUILDER/upgbuilder.sh
+++ UPGBUILDER/upgbuilder.sh
@@ -37,7 +37,7 @@ RULE_ORDER=(
 
 # -------------------- GLOBALS --------------------
 
-UPGBUILDER_VERSION="0.1.13"
+UPGBUILDER_VERSION="0.1.14"
 RAW_REPO_BASE="https://raw.githubusercontent.com/daroitgo/itgo-scripts"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
